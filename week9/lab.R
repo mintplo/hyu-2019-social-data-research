8bb107abafc72833bbe6f8ea27a5c95c3192456f
Jinman Lee <iam@mintplo.me>
2019-05-03 15:35:50 +0900

    3주차, 9주차 실습

---
+++ week9/lab.R
@@ -0,0 +1,20 @@
+# Data import
+data <- read.table("./ozone.data.txt", header = TRUE)
+
+# Plotting Data first
+plot(data)
+
+# Correlation
+data_cor <- cor(data)
+
+# Install Correlation Plotting Library
+install.packages("corrplot")
+library(corrplot)
+
+# Round floatting point to 2
+round(data_cor, 2)
+
+# Correlation Plotting with each options
+corrplot(data_cor, diag=F)
+corrplot(data_cor, method="number")
+corrplot(data_cor, method="number", type="lower", diag=F)
